4a946597199e2c8f3371d30772764937b070e1e7
Odilbek <odilbekjuraev07@gmail.com>
2023-02-03 14:20:38 +0500

    initial commmit

---
+++ android/app/src/main/kotlin/com/example/playeric/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.playeric
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Playeric',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Container(),
+    );
+  }
+}
