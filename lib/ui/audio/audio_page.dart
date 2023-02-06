fb59542d01d4369d0c113260bb9eee354e4ef72c
Odilbek <odilbekjuraev07@gmail.com>
2023-02-06 08:52:56 +0500

    some changes

--- lib/ui/audio/audio_page.dart
+++ lib/ui/audio/audio_page.dart
@@ -9,50 +9,53 @@ class AudioPage extends StatefulWidget {
 }
 
 class _AudioPageState extends State<AudioPage> {
+  @override
+  void initState() {
+    _init();
+    super.initState();
+  }
+
+  _init() async {
+    await player.setSource(AssetSource("assets/mp3/konsta.mp3"));
+  }
+
   final AudioPlayer player = AudioPlayer();
   bool isPlaying = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: const Color(0xFF0B0B0B),
       appBar: AppBar(
         title: const Text("Audio Page"),
       ),
-      body: Container(
-        child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: () {
-                  if (isPlaying) {
-                    player.pause();
-                  } else {
-                    player.play(AssetSource("assets/mp3/konsta.mp3"));
-                  }
-
-                  setState(() {
-                    isPlaying = !isPlaying;
-                  });
-                },
-                icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
-              ),
-              const SizedBox(
-                width: 50,
-              ),
-              IconButton(
-                onPressed: () {
-                  setState(() {
-                    isPlaying = false;
-                  });
-                },
-                icon: const Icon(
-                  Icons.stop,
-                  color: Colors.red,
-                ),
-              ),
-            ],
-          ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {
+                if (isPlaying) {
+                  player.pause();
+                } else {
+                  player.play(AssetSource("assets/mp3/konsta.mp3"));
+                }
+                setState(() {
+                  isPlaying = !isPlaying;
+                });
+              },
+              icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+            ),
+            const SizedBox(width: 50),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  isPlaying = false;
+                });
+              },
+              icon: const Icon(Icons.stop, color: Colors.red),
+            ),
+          ],
         ),
       ),
     );
